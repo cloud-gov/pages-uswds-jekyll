bf9e85315a7fecd927fc27a81cdb32a0da35b036
Eddie A Tejeda <eddie.tejeda@gmail.com>
2019-11-25 16:49:51 -0500

    add build script in ruby to be cross platform

---
+++ build.rb
@@ -0,0 +1,19 @@
+require 'fileutils'
+
+
+
+case ARGV.first
+when "clean"
+  puts "> cleaning..."
+  FileUtils.rm_rf('assets/uswds')
+  FileUtils.rm_rf('_site')
+when "uswds"
+  puts "> building USWDS..."
+  FileUtils.mkdir_p 'assets/uswds'
+  FileUtils.cp_r "node_modules/uswds/dist/css",   "assets/uswds/"
+  FileUtils.cp_r "node_modules/uswds/dist/img",   "assets/uswds/"
+  FileUtils.cp_r "node_modules/uswds/dist/fonts", "assets/uswds/"
+  FileUtils.cp_r "node_modules/uswds/dist/js",    "assets/uswds/"
+else
+  "Not an option"
+end
