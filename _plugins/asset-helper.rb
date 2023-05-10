0e5e38c9794b992a01a3912b2fb53ae727cf7602
Drew Bollinger <drew.bollinger@gsa.gov>
2023-05-10 15:24:14 -0400

    add autoprefixer plugin

--- _plugins/asset-helper.rb
+++ _plugins/asset-helper.rb
@@ -1,13 +1,13 @@
 module AssetHelper
     def self.process(site, payload)
-      return if @processed
-      system "cp -r node_modules/uswds/dist/js/ assets/js/"
-      system "cp -r node_modules/uswds/dist/img/ assets/uswds/img/" 
-      system "cp -r node_modules/uswds/dist/fonts/ assets/uswds/fonts/"
-      @processed = true
+        return if @processed
+        system "cp -r node_modules/uswds/dist/js/ assets/js/"
+        system "cp -r node_modules/uswds/dist/img/ assets/uswds/img/" 
+        system "cp -r node_modules/uswds/dist/fonts/ assets/uswds/fonts/"
+        @processed = true
     end
-  end
+end
   
-  Jekyll::Hooks.register :site, :post_write do |site, payload|
+Jekyll::Hooks.register :site, :post_write do |site, payload|
     AssetHelper.process(site, payload)
-  end
+end
