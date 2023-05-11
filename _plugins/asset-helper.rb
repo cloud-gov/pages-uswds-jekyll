19452806b3d2ecf8f284ace3f2c1407b5c887a26
Drew Bollinger <drew.bollinger@gsa.gov>
2023-05-11 18:01:32 -0400

    use ruby stdlib rather than system call

--- _plugins/asset-helper.rb
+++ _plugins/asset-helper.rb
@@ -1,9 +1,11 @@
+require 'fileutils'
+
 module AssetHelper
     def self.process(site, payload)
         return if @processed
-        system "cp -r node_modules/uswds/dist/js/ assets/js/"
-        system "cp -r node_modules/uswds/dist/img/ assets/uswds/img/" 
-        system "cp -r node_modules/uswds/dist/fonts/ assets/uswds/fonts/"
+        FileUtils.copy_entry('node_modules/uswds/dist/js/', 'assets/js/')
+        FileUtils.copy_entry('node_modules/uswds/dist/img/', 'assets/uswds/img/')
+        FileUtils.copy_entry('node_modules/uswds/dist/fonts/', 'assets/uswds/fonts/')
         @processed = true
     end
 end
