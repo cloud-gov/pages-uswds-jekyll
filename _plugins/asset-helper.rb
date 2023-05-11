611975ae588e2410627fcf445709a2a46ab2389d
Drew Bollinger <drew.bollinger@gsa.gov>
2023-05-11 18:58:12 -0400

    fix uswds asset copying

--- _plugins/asset-helper.rb
+++ _plugins/asset-helper.rb
@@ -3,13 +3,13 @@ require 'fileutils'
 module AssetHelper
     def self.process(site, payload)
         return if @processed
-        FileUtils.copy_entry('node_modules/uswds/dist/js/', 'assets/js/')
-        FileUtils.copy_entry('node_modules/uswds/dist/img/', 'assets/uswds/img/')
-        FileUtils.copy_entry('node_modules/uswds/dist/fonts/', 'assets/uswds/fonts/')
+        FileUtils.cp_r('node_modules/uswds/dist/js/', 'assets/', verbose: true)
+        FileUtils.cp_r('node_modules/uswds/dist/img/', 'assets/uswds/', verbose: true)
+        FileUtils.cp_r('node_modules/uswds/dist/fonts/', 'assets/uswds/', verbose: true)
         @processed = true
     end
 end
   
-Jekyll::Hooks.register :site, :post_write do |site, payload|
+Jekyll::Hooks.register :site, :after_reset do |site, payload|
     AssetHelper.process(site, payload)
 end
